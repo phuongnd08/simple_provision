e69e70736aeee67ecd00760f37d4a8a29dcfe694
Phuong Nguyen <phuongnd08@gmail.com>
2014-05-07 14:14:06 +0700

    Only do configure

--- lib/fucking_scripts_digital_ocean/server.rb
+++ lib/fucking_scripts_digital_ocean/server.rb
@@ -9,41 +9,6 @@ module FuckingScriptsDigitalOcean
       @connection, @options = connection, options
     end
 
-    def bootstrap
-      build
-      configure
-    end
-
-    def build
-      raise "Build is not supported yet"
-      @server = connection.servers.create(
-        image_id: options.fetch(:image),
-        flavor_id: options.fetch(:size),
-        key_name: options.fetch(:key_name),
-        tags: { "Name" => name },
-        groups: options.fetch(:security_groups),
-        private_key_path: options.fetch(:private_key_path)
-      )
-      print "Creating #{options.fetch(:size)} from #{options.fetch(:image)}"
-
-      server.wait_for do
-        print "."
-        ready?
-      end
-
-      puts "ready!"
-      puts ""
-
-      print "Waiting for ssh access"
-
-      server.wait_for do
-        print "."
-        sshable?
-      end
-
-      puts "#{server.dns_name} ready!"
-    end
-
     def configure
       get(options[:droplet_name]) if server.nil?
       raise NoServerSelected, "Unable to find server. Try specifying the server ID." if server.nil?
